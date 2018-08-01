edddeb7dd6cc1ab46e862af751d33797e55e1432
Aaron Chafetz <achafetz@usaid.gov>
2018-08-01 09:35:58 -0400

    add dependencies

--- Documents/test_pulls.R
+++ Documents/test_pulls.R
@@ -1,4 +1,9 @@
-datimvalidation::loadSecrets()
+library(tidyverse)
+library(jsonlite)
+library(httr)
+library(datimvalidation)
+
+loadSecrets()
 
 baseurl <- "https://pisa.datim.org/"
 url <- paste0(baseurl, "api/26/analytics.json?dimension=CKTkg8dLlr7:OxLSARiuCa8;KFZG3Lc0XgW;CeWDIASZw2r;YvO0x3N8iPk;qqV9vdPM2CB;QtnU6PoZwTj;mmef9WanhN1;jSX8elpqcD2;MZYo6lCaIhr;fTuReSq9mrJ;N3ZPt01yI74;UQJZLPV5BcW;Q8GqLayzQHh;sk2aTYKnZNz;y4f2Qs5jnv8;yQ9lXE1Sdm0&dimension=LxhLO68FcXm:f5IPTM7mieH&dimension=ou:LEVEL-3;PqlFzhuPcF1&filter=pe:2018Q1&displayProperty=SHORTNAME&skipMeta=false&hierarchyMeta=true")
