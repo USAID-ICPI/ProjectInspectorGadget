a8dbc5f2f11c5844e52773379d5ab67f53d6140f
Aaron Chafetz <achafetz@usaid.gov>
2018-08-01 16:59:46 -0400

    update test to pull list of key hts dataelements

--- Documents/test_pulls.R
+++ Documents/test_pulls.R
@@ -19,8 +19,32 @@
 
 # HTS - Validation --------------------------------------------------------
 
+# - 1. Only one disaggregation type is used for Age/Sex/Result
+# - 2. *Numerator = Sum of all modality (Excludes KP) 
+# - 3. HTS_TST_POS = sum of HTS_TST_POS for each modality 
+# - 4. All sites with targets reported results.
+# - 5. Sites with results in previous quarters reported results this quarter (same FY)
+# - 6. HTS _TST_POS (ANC) >= PMTCT_STAT (New Positives)
+
+  #create list of all relevant HTS indicators to pull
+    deurl <- "api/dataElements?paging=false&filter=name:like:HTS_TST"
+    
+    lst_hts <- 
+      pull_analytics(deurl, extract = "dataElements") %>% 
+      dplyr::filter(stringr::str_detect(displayName, "HTS_TST \\(N, (DSD|TA)\\):") | stringr::str_detect(displayName, "\\/(Age|Age Aggregated)\\/Sex\\/Result\\):")) %>% 
+      dplyr::pull(id) %>% 
+      paste0(collapse = ";")
+    
+    
   #HTS pivot table created in DATIM
-    pivoturl <- "api/26/analytics.json?dimension=dx:K6f6jR0NOcZ;FJSew4Ks0j3&dimension=ou:LEVEL-4;PqlFzhuPcF1&filter=pe:2018Q1&displayProperty=NAME&outputIdScheme=NAME"
-  
+    pivoturl <- paste0("api/26/analytics.json?dimension=dx:", lst_hts,"&dimension=ou:LEVEL-4;PqlFzhuPcF1&filter=pe:2018Q1&displayProperty=NAME&outputIdScheme=NAME")
+    
   #HTS API pull
     api_hts <- pull_analytics(pivoturl)
+  
+  #clean up data element
+    api_hts <- dismember(api_hts) %>% 
+      dplyr::select(org_unit, dataelement, indicator, disaggregate, numeratordenom, indicatortype, resulttarget, value)
+    
+  # Check #2
+   
